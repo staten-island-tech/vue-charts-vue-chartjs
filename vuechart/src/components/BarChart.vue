e8c16bef46797d6b25154c05f4461433f0070c21
jasminew13 <jasminewu101@gmail.com>
2024-03-05 10:18:59 -0500

    r

---
+++ vuechart/src/components/BarChart.vue
@@ -0,0 +1,35 @@
+<template>
+    <Bar
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"></Bar>
+ <div class="container" v-if="loaded" :data="chartData">  </div>
+  
+  </template>
+  
+  <script>
+  import {Bar } from "vue-chartjs"
+  export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: {
+        labels: ["b", "q", "m"]
+    }
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('https://data.cityofnewyork.us/resource/uiay-nctu.json')
+      this.chartdata = userlist
+
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+  </script>
