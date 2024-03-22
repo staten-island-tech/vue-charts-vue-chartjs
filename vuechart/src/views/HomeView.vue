f9368dc4b76cdbe5cd18a93524ae9d632fed1d7e
jasminew13 <jasminewu101@gmail.com>
2024-03-22 10:11:52 -0400

    Update HomeView.vue

--- vuechart/src/views/HomeView.vue
+++ vuechart/src/views/HomeView.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="thing">
-    ahdofs
-  </div>
+  <div class="container">
+
+    <div class="card">
+        <p><h2 class="organization">{{orgname}}</h2></p> 
+        <p><h3 class="street">{{appronstre}}      </h3></p>
+        <h2 class="theborough">{{boroughname}}</h2>
+      </div>
+
+    </div>
+
 </template>
 
 <script setup>
+const URL = `https://data.cityofnewyork.us/resource/uiay-nctu.json`
+async function getData(){
+  try {
+      const response = await fetch(URL);
+      if (response.status != 200) {
+          throw new Error (response.statusText);
+      }
+      const newcards = await response.json();
+      console.log(newcards.boroughname);
+  
+return newcards.appronstre, newcards.boroughname, newcards.orgname;
+  } catch (error) {
+      console.log(error,"uh oh");
+  }
+}
 
+
+
+getData(URL)
 </script>
 
 <style scoped>
