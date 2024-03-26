1f4eeb64afe36f24fc34908554d1b4a3d9db8793
yuhanz1 <yuhanz9149@gmail.com>
2024-03-26 10:12:07 -0400

    sd

--- project/src/components/PieSomething.vue
+++ project/src/components/PieSomething.vue
@@ -3,26 +3,43 @@
     </div>
 </template>
 
-<script setup>
+<script>
 import { ref, onMounted } from 'vue';
 const streets = ref('')
+
+export let manHatt = ref([]);
+export let broOklyn = ref([]);
+export let broNx = ref([]);
+export let queEns = ref([]);
+export let staTenis = ref([]);
+
 async function getData(){
   let res = await fetch('https://data.cityofnewyork.us/resource/uiay-nctu.json')
   let fdata = await res.json()
   streets.value = fdata.results;
-  console.log(fdata[32]);
+  console.log(fdata[8]);
+
+  fdata.forEach(el => {
+  if(fdata.appronstre === "Brooklyn"){
+    broOklyn.value.push(el);
+    console.log(broOklyn.value.length);
+  } else if(fdata.appronstre === "Staten Island"){
+    staTenis.value.push(el);
+  } else if(fdata.appronstre === "Manhattan"){
+    manHatt.value.push(el);
+  } else if(fdata.appronstre === "Queens"){
+    queEns.value.push(el);
+  } else {
+    broNx.value.push(el);
+  }
+  });
+
+  console.log(broNx.length);
+  console.log(broOklyn.length);
+  console.log(staTenis.length);
 }
+
 onMounted(()=>{
   getData()
 })
-
-// empty arrays for each brough -> push those with the specific borough name into the corres. array; then array.length it and just use that data for the chartcongfig thing
-//then. using the data, the "data" of the chartconjig gets updated . or you can use filter. filter(jekferjnf).uerfherif ewwjd = jsdskjdf ejdwen and then you just kinda array.length again
-let manHatt = [];
-let broOklyn = [];
-let BroNx = [];
-let queEns = [];
-let staTenis = [];
-
-
 </script>
--- project/src/components/chartConfig.ts
+++ project/src/components/chartConfig.ts
@@ -1,9 +1,12 @@
+import { manHatt, broOklyn, broNx, queEns, staTenis } from "./PieSomething.vue"
+
 export const data = {
   labels: ['Staten Island', 'Brooklyn', 'Manhattan', 'Queens', 'Bronx'],
   datasets: [
     {
       backgroundColor: ['#AC92EB', '#4FC1E8', '#A0D568', '#FFCE54', '#ED5564'],
-      data: [14, 181, 147, 67, 12]
+      data: [manHatt, broOklyn, broNx, queEns, staTenis],
+      //data: [10,10,10,10,10],
     }
   ]
 }  
