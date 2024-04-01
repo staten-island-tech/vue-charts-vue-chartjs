0193e93eb5ff21d5a0da2bc52653c5f99f4406b6
jasmine <jasminewu101@gmail.com>
2024-04-01 09:04:12 -0400

    n

---
+++ vuechar/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+
+<template>
+<div class="card" :key="street">
+{{ street }}
+</div>
+</template>
+<script>
+const URL = "https://data.cityofnewyork.us/resource/uiay-nctu.json";
+async function getData(){
+  try {
+      const response = await fetch(URL);
+      if (response.status != 200) {
+          throw new Error (response.statusText);
+      }
+      const newcards = await response.json();
+      console.log(newcards)
+      const street = newcards.appronstre
+      const borough = newcards.boroughname
+    } catch (error) {
+      console.log(error,"uh oh");
+  }
+}
+</script>
