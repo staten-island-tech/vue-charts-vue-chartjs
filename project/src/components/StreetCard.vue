142071ca4800d706805e7530e4ce2bc079163944
yuhanz1 <yuhanz9149@gmail.com>
2024-04-01 21:00:48 -0400

    herfbjehwekd

--- project/src/components/StreetCard.vue
+++ project/src/components/StreetCard.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
-    <h2> {{ streets.appronstre }}</h2>
+  <div class="card">
+    <h2>{{ streets }}</h2>
+    <h3>{{ timeO }}</h3>
+    <h3>{{ borough }}</h3>
   </div>
 </template>
 
-<script>
-const props = {
-  streets: Object,
-  borough: Object,
-}
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  streets: String,
+  timeO: String,
+  borough: String,
+});
+
+console.log(props);
 </script>
 
 <style scoped>
-
+.card{
+  background-color: #f0ede8;
+  padding: 3%;
+  margin: 1%;
+}
+h2{
+  font-weight: bold;
+}
 </style>
--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,39 +1,41 @@
 <template>
-  <main>
-    <h2> This is the home page. </h2>
-    <p></p>
-  </main>
+  <div class="container">
+    <StreetCard v-for="(street) in streets" 
+      :key="street.object_id" 
+      :streets="street.appronstre" 
+      :timeO="street.apprdayswe" 
+      :borough="street.boroughname"/>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import StreetCard from "../components/StreetCard.vue";
 
-const streets = ref('')
-async function getData(){
-  let res = await fetch('https://data.cityofnewyork.us/resource/uiay-nctu.json')
-  let fdata = await res.json()
-  streets.value = fdata.results
-  console.log(fdata);
-  console.log(fdata.length);
-  console.log(fdata[241].appronstre);
-  console.log(fdata[43].orgname);
+const streets = ref('');
 
-/*   let i = 0;
-  for(i=0; i<fdata.length; i++){
-    if(fdata[i].appronstre.value === "Brooklyn"){
-      broOklyn.push(fdata[i]);
-    } else if(fdata[i].appronstre.value === "Staten Island"){
-      staTenis.push(fdata[i]);
-    } else if(fdata[i].appronstre.value === "Bronx"){
-      broNx.push(fdata[i]);
-    } else if(fdata[i].appronstre.value === "Manhattan"){
-      manHatt.push(fdata[i]);
-    } else {
-      queEns.push(fdata[i]);
-    }
-  } */
+async function getData() {
+  try {
+    const res = await fetch('https://data.cityofnewyork.us/resource/uiay-nctu.json');
+    const data = await res.json();
+    streets.value = data;
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
-onMounted(()=>{
-  getData()
-})
+
+onMounted(() => {
+  getData();
+});
 </script>
+
+<style>
+.container {
+  margin: 2% auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
